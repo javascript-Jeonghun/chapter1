44886d7bd07bc028fab50949f7a4472e2a943573
seoul2015 <seoul2015.11.14@gmail.com>
2022-07-07 00:38:03 +0900

    add button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,12 @@
-alert('This is work');
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+
+function add() {
+  currentResult = currentResult +  parseInt(userInput.value);
+  outputResult(currentResult, '');
+}
+
+addBtn.addEventListener('click', add);
+
